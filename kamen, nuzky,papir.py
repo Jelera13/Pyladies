0c327a5388a885cd374afa0bd4436844043530b0
Vera B <117481897+Jelera13@users.noreply.github.com>
2022-11-06 09:01:31 +0100

    Kamen,nuzky,papir

---
+++ kamen, nuzky,papir.py
@@ -0,0 +1,19 @@
+from random import choice
+# rozhodnuti = "nic"
+# pocitac = ("kamen", "nuzky", "papir")
+
+# while rozhodnuti != "konec":
+#     tah_cloveka = input("Jak táhneš - kamen, nuzky nebo papir?")
+#     tah_pocitace = choice(pocitac)
+
+#     print("Tah počítače je:",tah_pocitace)
+#     print("Tvůj tah je:",tah_cloveka)
+
+#     if tah_cloveka == tah_pocitace:
+#         print("Plichta")
+#     elif (tah_cloveka == "kamen" and tah_pocitace == "papir") or (tah_cloveka == "nuzky" and tah_pocitace == "kamen") or (tah_cloveka == "papir" and tah_pocitace == "nuzky"):
+#         print("Počítač vyhrál")
+#     else:
+#         print("Vyhrál jsi!")
+    
+#     rozhodnuti = input("Pokud chceš hru ukončit, napiš -konec-, pokud ne zmáčkni enter:")
